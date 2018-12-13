d11b49fa92de5a858d8a047339d547954cb50880
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-12-12 19:56:40 -0600

    Add 404 page routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Players from './Players';
 import Teams from './Teams';
@@ -11,9 +11,12 @@ class App extends Component {
       <Router>
         <div>
           <Navbar />
-          <Route path="/" exact component={Home} />
-          <Route path="/players" component={Players} />
-          <Route path="/teams" component={Teams} />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/players" component={Players} />
+            <Route path="/teams" component={Teams} />
+            <Route render={() => <h1 className="text-center">404</h1>} />
+          </Switch>
         </div>
       </Router>
     );
