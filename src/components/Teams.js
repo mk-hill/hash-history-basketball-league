b7a624dddcc2ab27e44a84bf9f3ee18e7f8c2a4a
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-12-19 18:59:25 -0600

    Display selected team details

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import { getTeamNames } from '../api';
+import { Route, Link } from 'react-router-dom';
+import TeamLogo from './TeamLogo';
+import Team from './Team';
 
 class Teams extends Component {
   state = {
@@ -30,6 +33,46 @@ class Teams extends Component {
         {!loading && location.pathname === '/teams' ? (
           <div className="sidebar-instruction">Select a Team</div>
         ) : null}
+
+        <Route
+          path={`${match.url}/:teamId`}
+          render={({ match }) => (
+            <div className="panel">
+              <Team id={match.params.teamId}>
+                {team =>
+                  team === null ? (
+                    <h1>Loading</h1>
+                  ) : (
+                    <div style={{ width: '100%' }}>
+                      <TeamLogo id={team.id} className="center" />
+                      <h1 className="medium-header">{team.name}</h1>
+                      <ul className="info-list row">
+                        <li>
+                          Established
+                          <div>{team.established}</div>
+                        </li>
+                        <li>
+                          Manager
+                          <div>{team.manager}</div>
+                        </li>
+                        <li>
+                          Coach
+                          <div>{team.coach}</div>
+                        </li>
+                      </ul>
+                      <Link
+                        className="center btn-main"
+                        to={`/${match.params.teamId}`}
+                      >
+                        {team.name} Team Page
+                      </Link>
+                    </div>
+                  )
+                }
+              </Team>
+            </div>
+          )}
+        />
       </div>
     );
   }
